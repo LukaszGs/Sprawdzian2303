9715c164af612d948903e186939cf036d35f6108
Unknown <lukaszgrochans@gmail.com>
2018-03-18 12:06:29 +0100

    Zad5

--- src/Zadanie5/main5.java
+++ src/Zadanie5/main5.java
@@ -4,7 +4,8 @@ public class main5 {
     public static void main(String[] args) {
         zadanie5 zad5 = new zadanie5();
         int[][] tablica = new int[5][5];
-                
+        int tablica1 [] = {1, 2, 3};
+
 
         for(int i=0; i< tablica.length; i++){
             for(int j=0; j<tablica[i].length; j++)
